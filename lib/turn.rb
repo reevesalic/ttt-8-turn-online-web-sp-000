48a0f211f76095f2c385e24752104f7a6e02492c
Alicia Reeves <reevesalic@gmail.com>
2020-06-09 14:03:19 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -44,7 +44,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    player(board, index, player(board))
+    move(board, index, player(board))
     display_board(board)
   else
     turn(board)
