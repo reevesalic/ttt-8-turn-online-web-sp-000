74fc95924e3c70765bd39744da2e1fe0c471bda2
Alicia Reeves <reevesalic@gmail.com>
2020-06-09 14:03:57 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,63 @@
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+def display_board(board)
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+
+def input_to_index(user_input = "X")
+  user_input.to_i - 1
+  end
+
+def move(board, index, win = "X")
+  board[index] = win
+  end
+
+
+def position_taken? (board, index)
+    if board[index] == "" || board[index] == " " || board[index] == nil
+      return false
+    else
+      return true
+  end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else
+    return false
+  end
+end
+display_board(board)
+
+def player(board)
+  turn_count(board) % 2 == 0? "X" : "O"
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  }
+  counter
+end
