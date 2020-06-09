5a83c2390523f4b5b089e30c1a4543ff4199da0d
Alicia Reeves <reevesalic@gmail.com>
2020-06-09 12:47:45 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+def display_board(board)
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+def input_to_index(user_input = "X")
+  user_input.to_i - 1
+  end
+
+def position_taken? (board, index)
+    if board[index] == "" || board[index] == " " || board[index] == nil
+      return false
+    else
+      return true
+  end
+end
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else
+    return false
+  end
+end
+def move(board, index, win = "X")
+        board[index] = win
+  end
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  input_to_index(user_input)
+end
